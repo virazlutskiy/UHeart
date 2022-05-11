be889c4470dbed0551816d0aed86e8e9e2635df8
Vladimir <volodyarazl@gmail.com>
2022-05-11 21:28:53 +0300

    New testings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:uheart/main.dart';
@@ -13,18 +13,37 @@ import 'package:uheart/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(ProviderScope(
+      child: App(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    //1. Проверяем, что загаловок на месте
+    expect(find.text('UHealth'), findsOneWidget);
+    //2. Поскольку дивайс не подключен, то должен быть целый список дивайсов рядом
+    expect(find.text('Device'), findsWidgets);
+    //3. Нажатие на наш дивайс
+    await tester.tap(find.text('Device uhealth'),);
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //4. Проверяем, что теперь на странице нет всех дивайсов
+    expect(find.text('Device'), findsNothing);
+    //5. Проверяем, что есть кнопка отключиться от дивайса
+    await tester.tap(find.text('Disconect'),);
+    await tester.pump();
+    //6. Проверяем, что снова на экране дивайсов
+    expect(find.text('Device'), findsWidgets);
+    //7. Нажатие на наш дивайс
+    await tester.tap(find.text('Device uhealth'),);
+    await tester.pump();
+    //8. Проверяем что один блок сатурации
+    expect(find.text('CO2'), findsOneWidget);
+    //9. Проверяем что один блок батареии
+    expect(find.text('battery'), findsOneWidget);
+    //10. Проверяем что один блок часто
+    expect(find.text('rate'), findsOneWidget);
+    //11. Проверяем что нет пустых данных
+    expect(find.text('[]'), findsNothing);
+    //12. Проверяем что можем отключиться
+    await tester.tap(find.text('Disconect'),);
+    await tester.pump();
   });
 }
